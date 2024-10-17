786536b2e83bf77b5c6305b6dacdf3b63aa2d4fd
luida <aidashahrestani@gmail.com>
2024-10-18 00:00:42 +0330

    Feat: Update DashboardView with statics data

--- finance/views.py
+++ finance/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from .models import *
 
 class DashboardView(View):
     template_name = 'dashboard.html'
 
-    def get(self, request): 
-        return render(request, self.template_name)
+    def get(self, request):
+        user = request.user
+
+        labels = list()
+        data = list()
+
+        queryset = Transaction.objects.filter(user=user, transaction_type="expense") \
+            .values('category') \
+            .annotate(total_outcome=Sum('amount')) \
+            .order_by('-total_outcome')[:5]
+
+        for transaction in queryset:
+            labels.append(transaction['category'])
+            data.append(transaction['total_outcome'])
+
+        context = {
+            'labels': labels,
+            'data': data
+        }
+
+        return render(request, self.template_name, context)
